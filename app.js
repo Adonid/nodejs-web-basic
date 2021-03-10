0ce1859a20a6e06e3d3f1372cc5a43e731f0f7b7
LeHieu <Xavan912@gmail.com>
2021-03-10 07:22:58 +0700

    Cau hinh thu muc tinh static cho ung dung

--- app.js
+++ app.js
@@ -2,26 +2,26 @@
 const express = require('express');
 // Config info
 const config = require('config');
-// Format request
-const bodyParser = require('body-parser');
-// User for VIEWS
-
-
-const app = express();
+// Use for route & controller
 const controllers = require(__dirname + '/apps/controllers');
+// Use express
+const app = express();
+
 
 // Dua req ve dang json va chi lam viec voi json
-app.use(bodyParser.json());
+app.use(express.json());
 // Set thu muc view cua ung dung
 app.set('views', __dirname + '/apps/views');
 // Su dung engine la ejs cho view - tra ve html dong cho res
 app.set('view engine', 'ejs');
-// them vao app
+// Cau hinh file static
+app.use('/static', express.static(__dirname + "/public"));
+// them vao app cac route & controller
 app.use(controllers);
 
-// lang nghe
-const host = config.get("server.host");
-const port = config.get("server.port");
+// lang nghe cong ket noi
+const host = config.get("server.host")||"http://localhost";
+const port = config.get("server.port")||3000;
 app.listen(port, host, () => {
-    console.log("Serve is running on port 3000");
+    console.log("Serve is running on port " +port);
 });
