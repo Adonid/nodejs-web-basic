f0871ef0482a070a90bf73da1da737b8418aed76
LeHieu <Xavan912@gmail.com>
2021-03-14 13:34:30 +0700

    Xay dung xong them 2 bang cat & posts. Tiep tuc xay dung 3 bang: postDetail, comments, replyComments

---
+++ migrations/20210314062250-create-posts.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      imgUrl: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      desc: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      authorId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categories',
+          key: 'id',
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('posts');
+  }
+};
