27f45e0bec5af939193b123a488e314082fc86d7
LeHieu <Xavan912@gmail.com>
2021-04-26 09:22:03 +0700

    Thay doi regex kiem tra so dien thoai VietNam

--- apps/common/regex.js
+++ apps/common/regex.js
@@ -9,8 +9,8 @@ const {Slug} = require('../helpers')
  */
 const username = str => (new RegExp("^(?=.*[a-z])(?=.*[0-9])(?=.{3,16})")).test(str) ? false : true
 const password = str => (new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,32})")).test(str) ? false : true
-const phoneNumber = str => (new RegExp("^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}")).test(str) ? false : true
-// const textNormal = str => (new RegExp("^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\(\)%\^&\*])(?=.{3,64})")).test(str) ? false : true
+// So dien thoai VietNamese
+const phoneNumber = str => (new RegExp("^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])(?=.*[0-9])(?=.{10})")).test(str) ? false : true
 
 const textNormal = str => {
     const slug = Slug.slugName(str)
