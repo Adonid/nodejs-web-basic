45e59ca17ed84c721004fb648f831822d12543cc
LeHieu <Xavan912@gmail.com>
2021-04-26 08:39:00 +0700

    Lay thong tin chi tiet cho chinh user

--- apps/controllers/blog/action/mySelf.js
+++ apps/controllers/blog/action/mySelf.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const router = express.Router()
-
+const {User} = require('../../../models')
+const {notices, bcrypt} = require('../../../common')
+const {
+    generalMiddleware
+} = require("../../../middleware")
 
 /**
- * Ly du lieu cho trang dashboard ADMIN
+ * GET - Lay thong tin cua chinh toi
+ * 
+ * @param {user} - nhu trong da Passport tra ve
+ * 
+ * @returns {user} object JSON
  * 
  */
-router.post('/', (req, res) => {
-    return res.status(201).json({msg: "Thuc hien cap nhat thong tin"})
+ router.get('/', async (req, res) => {
+    const {user} = req
+    const myself = await User.getUser(user)
+                             .then(user => user)
+                             .catch(err => err)
+    if(myself){
+        delete myself.password
+        const msg = notices.reqSuccess(myself)
+        return res.status(msg.code).json(msg)
+    }
+    return res.status(notices._500.code).json(notices._500)
 })
+
 
 module.exports = router
