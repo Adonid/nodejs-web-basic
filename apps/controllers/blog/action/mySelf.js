f4013c8b879b609c2de059d85b8ac81bcbeae3a0
LeHieu <Xavan912@gmail.com>
2021-04-26 09:36:33 +0700

    Bo bcrypt vi khong phai doi mat khau cho user login social

--- apps/controllers/blog/action/mySelf.js
+++ apps/controllers/blog/action/mySelf.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {User} = require('../../../models')
-const {notices, bcrypt} = require('../../../common')
+const {notices} = require('../../../common')
 const {
     generalMiddleware
 } = require("../../../middleware")
