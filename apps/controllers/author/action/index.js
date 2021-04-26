e749be51a171cb695be831ad8e0b31efbffe5783
LeHieu <Xavan912@gmail.com>
2021-04-26 10:05:07 +0700

    Xoa file user.js du thua trong chuc nang author

--- apps/controllers/author/action/index.js
+++ apps/controllers/author/action/index.js
@@ -1,18 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const dashboard = require('./dashboard')
-const users = require('./users')
 const myself = require('./mySelf')
 
 /**
  *  Route cho phep vao lay ra du lieu cho trang dashboard
  */
  router.use('/', dashboard)
-
-/**
- *  Route - danh sach cac users
- */
- router.use('/user-list', users)
 
 /**
  *  Route - Thong tin ca nhan
