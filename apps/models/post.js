11d6fd01d898a27140b7b3777e6c4160d2c59f1c
LeHieu <Xavan912@gmail.com>
2021-05-05 15:08:05 +0700

    Them moi 1 model post de thao tac voi cac bai viet. Them cac phuong thuc co ban dau tien: lay bai viet, chi tiet bai viet, tao moi bai viet

---
+++ apps/models/post.js
@@ -0,0 +1,169 @@
+const {Post, User, Category, PostsContent, CommentsPost, FavouritesPost} = require('../../models')
+const { Op } = require("sequelize")
+
+/** LAY 1 BAI VIET THEO CAC TRUONG
+ * 
+ * muc dich de xem cac tham so dua vao co dung la bai ton tai khong
+ * 
+ * @param obj options - {id: 12}...
+ * 
+ * @return boolean or OBJECT
+*/
+const getOnePost = async obj => {
+    try {
+        const data = await Post.findOne({
+            attributes: ['id', 'title', 'image', 'desc', 'readTime', 'active', 'authorId', 'categoryId'],
+            where: obj
+        })
+        // console.log(data)
+        return data ? data.dataValues : false
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+/** LAY CAC BAI VIET THEO GIOI HAN DAU VAO
+ * 
+ * muc dich de lay cac bai viet khi cuon trang
+ * 
+ * @param {offset=0, limit=8} - mac dinh lay 8 bai viet moi nhat
+ * 
+ * @return boolean or OBJECT
+*/
+const getPosts = async (offset=0, limit=8) => {
+    try {
+        const data = await Post.findAll({
+            attributes: ['id', 'title', 'image', 'desc', 'readTime', 'active'],
+            include: [
+                {
+                    model: User,
+                    attributes: ['id', 'name', 'fullName', 'avatar', 'roleId']
+                },
+                {
+                    model: Category,
+                    attributes: ['id', 'name', 'image', 'color']
+                },
+                {
+                    model: CommentsPost,
+                    as: 'comments',
+                    attributes: ['id', 'userId']
+                },
+                {
+                    model: FavouritesPost,
+                    as: 'favourites',
+                    attributes: ['id', 'userId']
+                },
+            ],
+            offset,
+            limit
+        })
+        // console.log(data)
+        return data.length ? data.dataValues : false
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+/** LAY CHI TIET BAI VIET
+ * 
+ * muc dich de lay du lieu ve bai viet cho nguoi dung DOC
+ * 
+ * @param {obj} - vd id: 1000
+ * 
+ * @return boolean or OBJECT
+*/
+const getDetailedPost = async obj => {
+    try {
+        const data = await Post.findOne({
+            attributes: ['id', 'title', 'image', 'desc', 'readTime', 'active'],
+            include: [
+                {
+                    model: User,
+                    attributes: ['id', 'name', 'fullName', 'avatar', 'roleId']
+                },
+                {
+                    model: Category,
+                    attributes: ['id', 'name', 'image', 'color']
+                },
+                {
+                    model: CommentsPost,
+                    as: 'comments',
+                    attributes: ['id', 'userId']
+                },
+                {
+                    model: FavouritesPost,
+                    as: 'favourites',
+                    attributes: ['id', 'userId']
+                },
+                {
+                    model: PostsContent,
+                    as: 'content',
+                    attributes: ['content']
+                },
+            ],
+            where: obj
+        })
+        // console.log(data)
+        return data ? data.dataValues : false
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+/** TAO MOI 1 NOI DUNG CHO BAI VIET
+ * 
+ * 
+ * @param {postId, content} = dataContent
+ * 
+ * @return {true | false}
+ */
+const createNewContent = async dataContent => {
+    const content = await PostsContent.create(dataContent)
+    .then(content => {
+        return content ? true : false
+    })
+    .catch(err => {
+        console.log(err)
+        return false
+    })
+    return content
+}
+
+/** CREATE NEW POST
+ * 
+ * Tao moi bai viet
+ * 
+ * @param {'title', 'image', 'desc', 'readTime', 'content', 'authorId', 'categoryId'} = dataPost
+ * 
+ * @return {true | false}
+ */
+const createNewPost = async dataPost => {
+    const {title, image, desc, readTime, content, authorId, categoryId} = dataPost
+    const postId = await Post.create({
+        title,
+        image,
+        desc,
+        readTime,
+        authorId,
+        categoryId,
+    })
+    .then(post => {
+        // console.log(post)
+        return post ? post.dataValues.id : false
+    })
+    .catch(err => {
+        console.log(err)
+        return false
+    })
+    return postId ? await createNewContent({postId, content}) : false
+}
+
+module.exports = {
+    getOnePost,
+    getPosts,
+    getDetailedPost,
+    createNewPost
+}
