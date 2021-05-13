0a562e931dad9fae2318df03c51fb75abc747807
LeHieu <Xavan912@gmail.com>
2021-05-13 08:19:16 +0700

    Dua ra cac chi muc cho chuc nang nguoi xem truy cap vao trang

--- apps/controllers/blog/default/index.js
+++ apps/controllers/blog/default/index.js
@@ -1,7 +1,35 @@
 const express = require('express')
 const router = express.Router()
+const home = require('./home')
 
+/**
+ *  CAC ROUTER DUOC PHEP TRUY CAP MAC DINH TU USER CHUA DUOC DANG NHAP | DA DANG NHAP ROI
+ */
 
+/**
+ *  ROUTE TRUY CAP VAO TRANG CHU
+ */
+router.use('/', home)
+
+/**
+ *  LAY CAC BAI VIET THEO DANH MUC - CO GIOI HAN KHI CUON LOAD
+ */
+ router.use('/archive-posts', archivePosts)
+
+ /**
+  * LAY CHI TIET THONG TIN BAI VIET
+  */
+ router.use('/single-post', singlePost)
+
+ /**
+  * XEM THONG TIN & CAC BAI VIET CUA TAC GIA
+  */
+ router.use('/author-post', authorPost)
+
+ /**
+  * XEM THONG TIN ABOUTUS
+  */
+ router.use('/about-us', aboutUs)
 
 
 module.exports = router
