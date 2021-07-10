ba4e724929be58896a9c0478ab2977048d848ab3
LeHieu <Xavan912@gmail.com>
2021-07-10 12:38:23 +0700

    Bo import User model

--- apps/controllers/admin/authentication/address.js
+++ apps/controllers/admin/authentication/address.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {User, Address} = require('../../../models')
+const {Address} = require('../../../models')
 const {notices} = require('../../../common')
 
 
