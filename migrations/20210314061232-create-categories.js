842195947b4ae4ce677ec1a37e1f398cd2bd5015
LeHieu <Xavan912@gmail.com>
2021-07-14 09:51:17 +0700

    Them 2 bang quan ly anh cho user & post la usreImage & postImage. Cau truc lai du lieu database cho phu hop voi cac truong

---
+++ migrations/20210314061232-create-categories.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      imageId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'postImages',
+          key: 'id',
+        }
+      },
+      color: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('categories');
+  }
+};
---
+++ migrations/20210315030442-create-replys-comment.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('replysComments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      commentId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'commentsposts',
+          key: 'id',
+        }
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        }
+      },
+      reply: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('replysComments');
+  }
+};
