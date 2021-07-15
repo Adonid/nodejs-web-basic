e9f2f50e1b144ebfea07be1aad2e3017022abf47
LeHieu <Xavan912@gmail.com>
2021-07-15 19:26:57 +0700

    export module

--- apps/services/index.js
+++ apps/services/index.js
@@ -2,10 +2,12 @@ const JwtStrategy = require('./jwt')
 const Passport  = require('./passport')
 const Mailer  = require('./mailer')
 const DriverGoogle  = require('./drivergoogle')
+const ImageMannager  = require('./uploadImage')
 
 module.exports={
     JwtStrategy,
     Passport,
     Mailer,
-    DriverGoogle
+    DriverGoogle,
+    ImageMannager
 }
