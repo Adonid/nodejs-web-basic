8478f611ec582a551a23dc65b987c46627fd7cad
LeHieu <Xavan912@gmail.com>
2021-07-22 10:21:50 +0700

    Return thong bao ve dung dinh dang

--- apps/controllers/admin/action/manager.js
+++ apps/controllers/admin/action/manager.js
@@ -13,15 +13,19 @@ const {notices} = require('../../../common')
  */
 router.post('/activities', async (req, res) => {
     const {id, email, active} = req.body
-    const user = await User.updateUser(
-        {active},
-        {id, email}
-    )
-    if(user){
-        const note = notices.reqSuccess(user)
-        return res.status(note.code).send(note)
+    try {
+        const user = await User.updateUser(
+            {active},
+            {id, email}
+        )
+        if(user){
+            const note = notices._203("Tài khoản", user)
+            return res.status(note.code).send(note)
+        }
+        return res.status(notices._500.code).send(notices._500)
+    } catch (error) {
+        return res.status(notices._500.code).send(notices._500)
     }
-    return res.status(notices._500.code).send(notices._500)
 })
 
 module.exports = router
