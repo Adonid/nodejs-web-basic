ba01ba0b4844bd9cef5fdcc3c297e3b5daa91819
LeHieu <Xavan912@gmail.com>
2021-07-23 21:44:12 +0700

    Them model thao tac voi bang image Post

--- apps/models/index.js
+++ apps/models/index.js
@@ -3,11 +3,13 @@ const Category = require('./category')
 const Post = require('./post')
 const Address = require('./address')
 const ImageUser = require('./imageUser')
+const ImagePost = require('./imagePost')
 
 module.exports={
     User,
     Category,
     Post,
     Address,
-    ImageUser
+    ImageUser,
+    ImagePost
 }
