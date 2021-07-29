b180e7c7b02a01c0df80febc04dffb826f00ee82
LeHieu <Xavan912@gmail.com>
2021-07-29 15:40:39 +0700

    Tao them bang chua cac DU LIEU PHAN TAN: policy, background image... cho cac trang

---
+++ migrations/20210729083350-create-distributed-data.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('distributedData', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      type: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: true,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('distributedData');
+  }
+};
