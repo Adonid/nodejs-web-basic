f9a68b43e3fc033bcbcd0eacc057277d54f46c1b
LeHieu <Xavan912@gmail.com>
2021-10-21 10:47:55 +0700

    Sua ten bang noi dung post

--- migrations/20210315011903-create-posts-content.js
+++ migrations/20210315011903-create-posts-content.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('postsContents', {
+    await queryInterface.createTable('posts_contents', {
       id: {
         allowNull: false,
         autoIncrement: true,
