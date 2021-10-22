f8f7c53de58ff545972789ed07296688b407be89
LeHieu <Xavan912@gmail.com>
2021-10-22 21:46:16 +0700

    Them truong type cho bang color icon

--- migrations/20211021020522-create-colors-icons.js
+++ migrations/20211021020522-create-colors-icons.js
@@ -9,15 +9,18 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       type: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       code: {
         type: Sequelize.STRING
       },
       alias: {
+        allowNull: false,
         type: Sequelize.STRING
       }
     });
